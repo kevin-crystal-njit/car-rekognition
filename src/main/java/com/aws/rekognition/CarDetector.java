c3bf5bf777df35cba3ae2a18138449f6e7334d35
EC2 Default User <ec2-user@ip-172-31-83-165.ec2.internal>
2025-03-07 08:28:57 +0000

    Added SendToSQS

--- src/main/java/com/aws/rekognition/CarDetector.java
+++ src/main/java/com/aws/rekognition/CarDetector.java
@@ -8,12 +8,14 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.util.List;
 
 public class CarDetector {
     private static final String BUCKET_NAME = "njit-cs-643";
-
+    private static final String SQS_QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/280014048542/car-image-indices";
     public static void main(String[] args) {
         RekognitionClient rekognitionClient = RekognitionClient.builder()
                 .region(Region.US_EAST_1)
@@ -21,6 +23,11 @@ public class CarDetector {
                 .build();
 
         S3Client s3Client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        SqsClient sqsClient = SqsClient.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
@@ -41,11 +48,11 @@ public class CarDetector {
             System.out.println("Analyzing file: " + fileName);
 
             // Build the Rekognition S3 object
-            software.amazon.awssdk.services.rekognition.model.S3Object rekognitionS3Object = 
-                software.amazon.awssdk.services.rekognition.model.S3Object.builder()
-                    .bucket(BUCKET_NAME)
-                    .name(fileName)
-                    .build();
+            software.amazon.awssdk.services.rekognition.model.S3Object rekognitionS3Object =
+                    software.amazon.awssdk.services.rekognition.model.S3Object.builder()
+                            .bucket(BUCKET_NAME)
+                            .name(fileName)
+                            .build();
 
             // Configure the image
             Image image = Image.builder()
@@ -65,11 +72,20 @@ public class CarDetector {
             boolean containsCar = response.labels().stream()
                     .anyMatch(label -> label.name().equalsIgnoreCase("Car"));
 
-            System.out.println("File: " + fileName + " - Contains car: " + containsCar);
+            if (containsCar) {
+                // Send the fileName to SQS
+                SendMessageRequest sendMsgRequest = SendMessageRequest.builder()
+                        .queueUrl(SQS_QUEUE_URL)
+                        .messageBody(fileName)
+                        .build();
+                sqsClient.sendMessage(sendMsgRequest);
+                System.out.println("Sent key to SQS: " + fileName);
+            }
         }
 
         rekognitionClient.close();
         s3Client.close();
+        sqsClient.close();
     }
 }
 
